129fc2dd5bc30e0bb3b2ac57f78e6f9e6abccb2a
wuzirui <wuzirui@tztyun.org>
2024-01-04 23:08:15 +0800

    [bugfix] adduser script

--- misc/add_user.sh
+++ misc/add_user.sh
@@ -10,13 +10,13 @@ if id -u ${NORMAL_USER} >/dev/null 2>&1; then
     exit 0
 fi
 
-mkdir /data
+mkdir -p /data
 useradd -md /data/${NORMAL_USER} -s /bin/bash ${NORMAL_USER}
-echo ${ADMIN_USER}:${ADMIN_USER}@123! | chpasswd
-chage -d 0 $ADMIN_USER
+echo ${NORMAL_USER}:${NORMAL_USER}@123 | chpasswd
+chage -d 0 $NORMAL_USER
 
-sudo -u ${ADMIN_USER} /usr/local/miniconda3/bin/conda init bash
-sudo -u ${ADMIN_USER} /usr/local/miniconda3/bin/conda config --set auto_activate_base false
-sudo -u ${ADMIN_USER} /usr/local/miniconda3/bin/conda config clean -i -y
-sudo -u ${ADMIN_USER} cp ./sources/.condarc /data/${ADMIN_USER}/.condarc
-sudo -u ${ADMIN_USER} conda run pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+sudo -u ${NORMAL_USER} /usr/local/miniconda3/bin/conda init bash
+sudo -u ${NORMAL_USER} /usr/local/miniconda3/bin/conda config --set auto_activate_base false
+#sudo -u ${NORMAL_USER} /usr/local/miniconda3/bin/conda config clean -i -y
+sudo -u ${NORMAL_USER} cp ./sources/.condarc /data/${NORMAL_USER}/.condarc
+sudo -u ${NORMAL_USER} /usr/local/miniconda3/bin/conda run pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple

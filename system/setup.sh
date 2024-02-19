ccc50d2aed513ed8accc08e1b56693d36edc0c86
wuzirui <wuzirui@tztyun.org>
2024-02-19 11:53:36 +0800

    update system monitor

--- system/setup.sh
+++ system/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir $HOME/.scripts
-mkdir $HOME/.log
-cp tmux_monitor.sh $HOME/.scripts/
-ln -s $PWD/wired_check.sh $HOME/.scripts/
-ln -s $PWD/wifi_check.sh $HOME/.scripts/
+mkdir /home/ubuntu/.scripts
+mkdir /home/ubuntu/.log
+cp tmux_monitor.sh /home/ubuntu/.scripts/
+ln -s $PWD/wired_check.sh /home/ubuntu/.scripts/
+ln -s $PWD/wifi_check.sh /home/ubuntu/.scripts/
 
 apt install ssmtp sharutils
 
